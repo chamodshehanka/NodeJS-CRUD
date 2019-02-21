5793c825ed905ee3f91c16272f1ba31c061e438b
Shehanka <hcsperera@outlook.com>
2019-02-22 00:26:31 +0530

    updated

--- app.js
+++ app.js
@@ -7,6 +7,47 @@ const path = require('path');
 const db = require('./db');
 const collection = 'todo';
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/getTodos', (req, res) => {
+    db.getDB().collection(collection).find({}).toArray((err, documents) => {
+        if(err){
+            console.log(err);
+        }else{
+            console.log(documents);
+            res.json(documents);
+        }
+    });
+});
+
+app.put('/:id', (req, res) => {
+    const todoID = req.param.id;
+    const userInput = req.body;
+
+    db.getDB.collection(collection).findOneAndUpdate({_id: todoID}, 
+        {$set: {todo : userInput.todo}}, 
+        {returnOriginal: false},
+        (err, res) => {
+            if(err){
+                console.log(err);
+            }else{
+                res.json(result);
+            }
+    });
+});
+
+app.post('/', (req,res) => {
+    const userInput = req.body;
+    db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json({result : result, document: result.ops[0]});
+        }
+    });
+});
 
 db.connect((err) => {
     if(err){
