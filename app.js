ba95f757852a555c2a65fa1b81b330dcccdd45a0
Shehanka <hcsperera@outlook.com>
2019-08-26 10:39:24 +0530

    update

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.get('/', (req, res) => {
 
 app.get('/getTodos', (req, res) => {
     db.getDB().collection(collection).find({}).toArray((err, documents) => {
-        if(err){
+        if (err) {
             console.log(err);
-        }else{
+        } else {
             console.log(documents);
             res.json(documents);
         }
@@ -26,34 +26,34 @@ app.put('/:id', (req, res) => {
     const todoID = req.param.id;
     const userInput = req.body;
 
-    db.getDB.collection(collection).findOneAndUpdate({_id: todoID}, 
-        {$set: {todo : userInput.todo}}, 
-        {returnOriginal: false},
+    db.getDB.collection(collection).findOneAndUpdate({ _id: todoID },
+        { $set: { todo: userInput.todo } },
+        { returnOriginal: false },
         (err, res) => {
-            if(err){
+            if (err) {
                 console.log(err);
-            }else{
+            } else {
                 res.json(result);
             }
-    });
+        });
 });
 
-app.post('/', (req,res) => {
+app.post('/', (req, res) => {
     const userInput = req.body;
     db.getDB().collection(collection).insertOne(userInput, (err, result) => {
-        if(err){
+        if (err) {
             console.log(err);
-        }else{
-            res.json({result : result, document: result.ops[0]});
+        } else {
+            res.json({ result: result, document: result.ops[0] });
         }
     });
 });
 
 db.connect((err) => {
-    if(err){
+    if (err) {
         console.log('Unable to connect!');
         process.exit(1);
-    }else{
+    } else {
         app.listen(3000, () => {
             console.log('Connected to database, app listing on port 3000');
         });
